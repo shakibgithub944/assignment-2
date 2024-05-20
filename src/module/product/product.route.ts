c0753d1cd847c4d710d2f07a4f815d0a3bff9535
shakib <shakibmiah554@gmail.com>
2024-05-21 00:09:04 +0600

    Product module done

--- src/module/product/product.route.ts
+++ src/module/product/product.route.ts
@@ -4,9 +4,14 @@ import productController from './product.controller';
 
 const router = express.Router();
 
+// Product routes start with /products
 router.post('/products', productController.createProduct);
 router.get('/products', productController.getAllProducts);
 router.get('/products/:productId', productController.getProductById);
 router.put('/products/:productId', productController.updateProductById);
 router.delete('/products/:productId', productController.deleteProductById);
+// product routes end
+
+
+
 export default router;
